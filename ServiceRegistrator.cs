263d5e5e203d930c2a4746a0ec45de0437a95f74
j4ckgrey <j4ckgrey@gmail.com>
2026-02-03 13:51:05 +0100

    v0.2.2.0: Fixed DI service registration for CacheService

--- ServiceRegistrator.cs
+++ ServiceRegistrator.cs
@@ -2,6 +2,7 @@ using MediaBrowser.Common.Configuration;
 using MediaBrowser.Controller;
 using MediaBrowser.Controller.Plugins;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Palco;
 
@@ -13,6 +14,11 @@ public class ServiceRegistrator : IPluginServiceRegistrator
     /// <inheritdoc />
     public void RegisterServices(IServiceCollection services, IServerApplicationHost applicationHost)
     {
-        services.AddSingleton<CacheService>();
+        services.AddSingleton<CacheService>(sp =>
+        {
+            var appPaths = sp.GetRequiredService<IApplicationPaths>();
+            var logger = sp.GetRequiredService<ILogger<CacheService>>();
+            return new CacheService(appPaths, logger);
+        });
     }
 }
